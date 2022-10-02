01517501e1240d6ac8f5d5158c54beaa8a4bbb07
Kyle <kylemar99@gmail.com>
2022-10-02 03:19:21 +0000

    checkout view

--- checkout/forms.py
+++ checkout/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from .models import Order
 
 
-class orderForm(forms.ModelForm):
+class OrderForm(forms.ModelForm):
     class Meta:
-        model = Order()
+        model = Order
         fields = ('full_name', 'email', 'phone_number', 'street_address1', 'street_address2', 'town_or_city', 'postcode', 'country', 'county',)
 
     def __init__(self, *args, **kwargs):
