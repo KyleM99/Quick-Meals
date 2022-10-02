361be4b27598f9757feabfad8a95874ec60ce7d6
Kyle <kylemar99@gmail.com>
2022-10-02 04:36:09 +0000

    tidy up

--- checkout/views.py
+++ checkout/views.py
@@ -9,8 +9,6 @@ import stripe
 
 
 def checkout(request):
-    #stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    #stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     bag = request.session.get('bag', {})
     if not bag:
@@ -20,12 +18,14 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
-    #stripe.api_key = stripe_public_key
-    #intent = stripe.PaymentIntent.create(
-        #amount=stripe_total,
-        #currency=settings.STRIPE_CURRENCY,
-    #)
-    #print(intent)
+    # stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    # stripe_secret_key = settings.STRIPE_SECRET_KEY
+    # stripe.api_key = stripe_public_key
+    # intent = stripe.PaymentIntent.create(
+    # amount=stripe_total,
+    # currency=settings.STRIPE_CURRENCY,
+    # )
+    # print(intent)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
